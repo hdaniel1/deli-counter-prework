7242d227bfb24eabbdec5f8c88b530d5bf876389
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-12 18:30:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,11 @@
 # Write your code here.
 katz_deli = []
-def line()
+def line(katz_deli)
+  new = []
+  if katz_deli.length > 0
+    katz_deli.each_with_index { |person, number|
+      new.push("#{number + 1}. #{person} ")
+    end 
+  return new
+    
+end
