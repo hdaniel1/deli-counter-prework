a9810bf9b6b16ae0836c121a758e2871c6d5453c
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-12 18:31:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,6 @@ def line(katz_deli)
     katz_deli.each_with_index { |person, number|
       new.push("#{number + 1}. #{person} ")
     end
-  return "The line is currently: #{new}""
+  return "The line is currently: #{new}"
 
 end
