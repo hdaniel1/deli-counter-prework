4f0a018fbd3c62d22fc7dab4fd4ebdaf06df8a00
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-12 18:33:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   new = []
   if katz_deli.length > 0
     katz_deli.each_with_index { |person, number|
-      new.push("#{number + 1}. #{person} ")
+      new.push("#{number + 1}. #{person}")
       return "The line is currently: #{new}"
     end
   else return "The line is currenlty empty."
