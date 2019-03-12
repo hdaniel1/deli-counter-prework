6b1a9c09b9f62db0b931b5cfdf5aea116d1ba3f3
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-12 18:34:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
       new.push("#{number + 1}. #{person}")
       return "The line is currently: #{new}"
     end
-  else return "The line is currenlty empty."
+  else return "The line is currently empty."
   end
 
 end
